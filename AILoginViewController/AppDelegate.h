4efe9c6d72c44f82cfc7bae6312934be5c87a59d
GRADUATION <izh.sever@gmail.com>
2014-12-18 08:25:05 +0300

    create Project

---
+++ AILoginViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AILoginViewController
+//
+//  Created by Alexander Ignition on 18.12.14.
+//  Copyright (c) 2014 Alexander Ignition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AILoginViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AILoginViewController
+//
+//  Created by Alexander Ignition on 18.12.14.
+//  Copyright (c) 2014 Alexander Ignition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
